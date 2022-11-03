0a4a730fa11a2a1038f16fad36a72a3fcd699350
chris-ortiz <christian.ortiz@adesso.de>
2022-11-03 10:43:02 +0100

    fixes

--- src/main/java/de/adesso/demo/G_StructuredConcurrency.java
+++ src/main/java/de/adesso/demo/G_StructuredConcurrency.java
@@ -2,7 +2,6 @@ package de.adesso.demo;
 
 import jdk.incubator.concurrent.StructuredTaskScope;
 
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -10,13 +9,13 @@ public class G_StructuredConcurrency {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-            Future<UUID> world = scope.fork(() -> {
+            Future<Void> world = scope.fork(() -> {
                 Thread.sleep(100);
                 System.out.println("World");
                 return null;
             });
 
-            Future<UUID> hello = scope.fork(() -> {
+            Future<Void> hello = scope.fork(() -> {
                 System.out.println("Hello");
                 return null;
             });
